a3af725f028187404f48e329d16ecadea306ac7b
ShivamSukhwal <shivam33@gmail.com>
2019-06-25 19:11:09 -0400

    Fully functioning

--- assets/app.js
+++ assets/app.js
@@ -1,17 +1,17 @@
 setInterval(updateClock, 1000);
 
 // 1. Initialize Firebase
-var config = {
+var firebaseConfig = {
   apiKey: "AIzaSyCCsfCHhc0XwxW7t9Yp-m54gTa0HLoWJKU",
   authDomain: "train-time-f2787.firebaseapp.com",
   databaseURL: "https://train-time-f2787.firebaseio.com",
   projectId: "train-time-f2787",
-  storageBucket: "",
+  storageBucket: "train-time-f2787.appspot.com",
   messagingSenderId: "719136475681",
   appId: "1:719136475681:web:aef937013ba466a3"
 };
 
-firebase.initializeApp(config);
+firebase.initializeApp(firebaseConfig);
 
 var database = firebase.database();
 
@@ -24,3 +24,85 @@ function updateClock() {
 }
 
 setInterval(updateClock, 1000);
+
+$("#add-train-btn").click(function(event) {
+  event.preventDefault();
+  var newTrain = $("#train-name-input")
+    .val()
+    .trim();
+  var newDestination = $("#destination-input")
+    .val()
+    .trim();
+  var newFirstTrain = $("#first-train-input")
+    .val()
+    .trim();
+  var newFrequency = $("#frequency-input")
+    .val()
+    .trim();
+
+  newObject = {
+    train: newTrain,
+    destination: newDestination,
+    firstTrain: newFirstTrain,
+    frequency: newFrequency
+  };
+
+  database.ref().push(newObject);
+
+  console.log(newObject.train);
+  console.log(newObject.destination);
+  console.log(newObject.firstTrain);
+  console.log(newObject.frequency);
+
+  alert("Train successfully added!");
+
+  $("#train-name-input").val("");
+  $("#destination-input").val("");
+  $("#first-train-input").val("");
+  $("#frequency-input").val("");
+});
+
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+  console.log(childSnapshot.val());
+
+  var newTrain = childSnapshot.val().train;
+  var newDestination = childSnapshot.val().destination;
+  var newFirstTrain = childSnapshot.val().firstTrain;
+  var newFrequency = childSnapshot.val().frequency;
+
+  console.log(newTrain);
+  console.log(newDestination);
+  console.log("FIRST TRAIN DEPARTED AT: " + newFirstTrain);
+  console.log("THE TRAIN ARRIVES EVERY " + newFrequency + " MINUTES");
+
+  var firstTrainConverted = moment(newFirstTrain, "hh:mm").subtract(1, "days");
+  console.log(firstTrainConverted);
+
+  var diffTime = moment().diff(moment(firstTrainConverted), "minutes");
+  console.log("DIFFERENCE IN TIME: " + diffTime);
+
+  var timeApart = diffTime % newFrequency;
+  console.log("MINUTES TO SUBTRACT FROM FREQUENCY: " + timeApart);
+
+  var minutesAway = newFrequency - timeApart;
+  console.log("MINUTES UNTIL TRAIN: " + minutesAway);
+
+  var nextArrival = moment().add(minutesAway, "minutes");
+  var nextArrival2 = moment(nextArrival).format("hh:mm");
+
+  console.log("ARRIVAL TIME: " + moment(nextArrival).format("hh:mm"));
+
+  $("#train-table > tbody").append(
+    "<tr><td>" +
+      newTrain +
+      "</td> <td>" +
+      newDestination +
+      "</td> <td>" +
+      newFrequency +
+      "</td><td>" +
+      nextArrival2 +
+      "</td><td>" +
+      minutesAway +
+      "</td></tr>"
+  );
+});
